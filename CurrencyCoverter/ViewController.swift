ba10680f0bbd64648402f87086ad9c343dfc1cc0
Hanting Chiang <y790930@gmail.com>
2019-05-10 08:35:46 +0900

    Initial Commit

---
+++ CurrencyCoverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyCoverter
+//
+//  Created by 翰廷 姜 on 2019/5/10.
+//  Copyright © 2019 Hanting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
